9a21d813822197aa28edfb79570938522275e8d9
joseheriberto17 <jose.heriberto@hotmail.es>
2025-01-17 09:32:30 -0500

    creacion de funciones con struct como argumentos

--- general/general.c
+++ general/general.c
@@ -6,38 +6,64 @@ struct cuenta
     float valor;
     char *nombre;
     long cell;
-} usuario;
+} usuario[5];
+
+void mostrarDatosPorValor(struct cuenta data[],int size);
+void mostrarDatosPorReferencia(struct cuenta *usuarioPtr,int size);
 
 int main()
 {
 
-    struct cuenta *usuarioPtr = &usuario;
+    struct cuenta *usuarioPtr = usuario;
 
     // inicializado valores
-    usuario.idCuenta = 21;
-    usuario.valor = 5000;
-    usuario.nombre = "jose heriberto";
-    usuario.cell = 3126849682;
+    usuario[0].idCuenta = 21;
+    usuario[0].valor = 5000;
+    usuario[0].nombre = "jose heriberto";
+    usuario[0].cell = 3126849682;
 
-    // usuario[1].idCuenta = 25;
-    // usuario[1].valor = 10000;
-    // usuario[1].nombre = "juan daniel";
-    // usuario[1].cell = 3214566789;
+    usuario[1].idCuenta = 25;
+    usuario[1].valor = 10000;
+    usuario[1].nombre = "juan daniel";
+    usuario[1].cell = 3214566789;
 
 
-    puts("impresion por valor");
-    printf("%8s: %s\n", "nombre", usuario.nombre);
-    printf("%8s: %2.2f$\n", "valor", usuario.valor);
-    printf("%8s: %d\n", "idCuenta", usuario.idCuenta);
-    printf("%8s: %ld\n", "celular", usuario.cell);
+    mostrarDatosPorValor(usuario,5);
 
     puts("");
+
+    mostrarDatosPorReferencia(usuarioPtr,5);
+    puts("");
+
+    usuarioPtr++;
+
+    mostrarDatosPorReferencia(usuarioPtr,5);
+    puts("");
+
+    usuarioPtr--;
+
+    mostrarDatosPorReferencia(usuarioPtr,5);
+
+    
+
+    return 0;
+}
+
+void mostrarDatosPorValor(struct cuenta data[],int size){
+
+    puts("impresion por valor");
+    printf("%8s: %s\n", "nombre", data[0].nombre);
+    printf("%8s: %2.2f$\n", "valor", data[0].valor);
+    printf("%8s: %d\n", "idCuenta", data[0].idCuenta);
+    printf("%8s: %ld\n", "celular", data[0].cell);
+}
+
+void mostrarDatosPorReferencia(struct cuenta *usuarioPtr,int size){
 
     puts("impresion por referencia");
     printf("%8s: %s\n", "nombre", usuarioPtr->nombre);
     printf("%8s: %2.2f$\n", "valor", usuarioPtr->valor);
     printf("%8s: %d\n", "idCuenta", usuarioPtr->idCuenta);
     printf("%8s: %ld\n", "celular", usuarioPtr->cell);
-
-    return 0;
 }
+
