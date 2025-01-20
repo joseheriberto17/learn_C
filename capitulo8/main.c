804e864cd43227c08cbd47ca665da41e43b47c89
The_heri24 <jose.heriberto@hotmail.es>
2025-01-19 22:59:02 -0500

    .

--- capitulo8/main.c
+++ capitulo8/main.c
@@ -3,33 +3,58 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <limits.h>
+#include <string.h>
 
 void obtenerInfoCadena(void);
+void conversionDeCadena(void);
+void usoDeFuncionesEstandar(void);
 
 // uso de la biblioteca de funcion de caracteres en C
 int main(void)
 {
     // obtenerInfoCadena();
+    // void conversionDeCadena(void)
+    // void usoDeFuncionesEstandar(void);
 
+    char data[]="12";
 
-    const char *nPtr = "25.2334asd";
-    char *nnPtr; // puntero para mostrar el resto de la cadena
+    printf("%s",data);
 
-    double d = strtod(nPtr, &nnPtr);
-    printf("%20s: %f, restos: %s\n","valor double", d, nnPtr);
+    char cadenaVacia1[20] = "";
+    char cadenaActual1[20] = "123456789012345678";
 
-    long int ld = strtol(nPtr, &nnPtr,10);
-    printf("%20s: %ld, restos: %s\n","valor long int", ld,nnPtr);
+    puts("uso de strcpy");
+    printf("cadenaVacia: %s, cadenaAltual: %s\n",cadenaVacia1,cadenaActual1);
+    strcpy(cadenaVacia1, cadenaActual1);
+    printf("cadenaVacia: %s, cadenaAltual: %s\n",cadenaVacia1,cadenaActual1);
+    puts("");
 
-    unsigned long lu = strtoul(nPtr, &nnPtr,10);
-    printf("%20s: %lu, restos: %s\n","valor unsigned long",lu,nnPtr);
+    char cadenaVacia2[10] = {"abcdefg"};
+    char cadenaActual2[20] = {"123456789012345678"};
 
-    // printf("getchar: %x\n",getchar());
+    puts("uso de strncpy");
+    printf("cadenaVacia: %s, cadenaAltual: %s\n", cadenaVacia2, cadenaActual2);
+    strncpy(cadenaVacia2, cadenaActual2, 5);
+    printf("cadenaVacia: %s, cadenaAltual: %s\n", cadenaVacia2, cadenaActual2);
+    puts("");
 
-    char sentence[7] = "";
-    fgets(sentence,7,stdin);
+    char cadenaA1[20]="abcdefghi";
+    char cadenaB1[10]="123456789";
 
-    printf("%s",sentence);
+    puts("uso de strcat");
+    printf("cadenaA: %s, cadenaB: %s\n", cadenaA1, cadenaB1);
+    strcat(cadenaA1, cadenaB1);
+    printf("cadenaA: %s, cadenaB: %s\n", cadenaA1, cadenaB1);
+    puts("");
+
+    char cadenaA2[20]="abcdefghi";
+    char cadenaB2[10]="123456789";
+
+    puts("uso de strncat");
+    printf("cadenaA: %s, cadenaB: %s\n", cadenaA2, cadenaB2);
+    strncat(cadenaA2, cadenaB2,3);
+    printf("cadenaA: %s, cadenaB: %s\n", cadenaA2, cadenaB2);
+    puts("");
 
     return 0;
 }
@@ -50,4 +75,43 @@ void obtenerInfoCadena(void)
     printf("%31s: %d\n", "es un caracter especial", isgraph('d'));
     printf("%31s: %d\n", "pasar a minuscula", tolower('A'));
     printf("%31s: %d\n", "pasar a mayuscula", toupper('a'));
+}
+
+void conversionDeCadena(void)
+{
+    const char *nPtr = "25.2334asd";
+    char *nnPtr; // puntero para mostrar el resto de la cadena
+
+    double d = strtod(nPtr, &nnPtr);
+    printf("%20s: %f, restos: %s\n", "valor double", d, nnPtr);
+
+    long int ld = strtol(nPtr, &nnPtr, 10);
+    printf("%20s: %ld, restos: %s\n", "valor long int", ld, nnPtr);
+
+    unsigned long lu = strtoul(nPtr, &nnPtr, 10);
+    printf("%20s: %lu, restos: %s\n", "valor unsigned long", lu, nnPtr);
+}
+
+void usoDeFuncionesEstandar(void)
+{
+    char sentence[20] = "";
+
+    // leer una linea de texto fgets
+    fgets(sentence, 7, stdin);
+    printf("cadena: %s", sentence);
+
+    // imrpmiri un caracter
+    printf("putchar[1]: ");
+    putchar(sentence[1]);
+
+    // imprimir una linea con getchar
+    int c = 0;
+    putc('\n', stdout);
+    while (c != '\n')
+    {
+        c = getchar();
+        printf("%c", c);
+    }
+
+    // sprintf(sentence, "integer:%6d\ndouble:%7.2f", 1234, 1.456);
 }
