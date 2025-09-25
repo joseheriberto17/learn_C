b2fd3736538626b975b3dbe93bb7e058f90c4490
The_heri24 <jose.heriberto@hotmail.es>
2025-09-25 06:15:34 -0500

    agregacion de capitulo 1 de "el gran libro de programacion en C"

---
+++ capitulo1/main.c
@@ -0,0 +1,12 @@
+// Directiva del preprosesador. (Standard Input Output)
+// ".h" abr. de header
+#include <stdio.h>
+
+// Los argumento de main no son obligatorios, a menos que quiera ejecutar el programa con argumentos
+int main(int argc, char *argv[])
+{
+    printf("hola mundo");
+
+    // Es posible que no se nesecite definir de forma explicita
+    return 0;
+}
